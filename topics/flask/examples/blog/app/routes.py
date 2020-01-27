e48ce09060b7cad6f13f42705aa8cb1d5c2b65fd
Adam Carter <mail@adam-carter.com>
2020-01-26 19:11:48 -0800

    added some sample starter code for tomorrow.

--- topics/flask/examples/blog/app/routes.py
+++ topics/flask/examples/blog/app/routes.py
@@ -1,10 +1,17 @@
 from app import app
+from markdown import markdown
+from flask import render_template_string
 
 #home page
 @app.route("/")
 def home():
     return "<h1>My Blog</h1>"
 
-@app.route("/about")
-def about():
-    return "<h1>About Me</h1>"
+@app.route("/<view_name>")
+def about(view_name):
+    about_markdown = ""
+    with open('app/views/'+ view_name +'.md') as contents:
+        about_markdown = contents.read()
+    about_html = markdown(about_markdown)
+    return render_template_string(about_html, view_name=view_name)
+    
---
+++ topics/flask/examples/blog/readme.md
@@ -0,0 +1,8 @@
+#virtual environment setup steps (Windows computers)
+
+```
+python -m venv venv
+.\venv\Scripts\Activate.ps1
+pip install flask
+pip install Markdown
+```
